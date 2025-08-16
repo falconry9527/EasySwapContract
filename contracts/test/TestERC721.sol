cc17ff9293902f9bb0fe4ca1a643632f6ca88bca
conry <kequan888@gmail.com>
2025-08-16 14:58:06 +0800

    TestERC721

--- contracts/test/TestERC721.sol
+++ contracts/test/TestERC721.sol
@@ -1,23 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestERC721 is ERC721Enumerable, Ownable {
-    string private _tokenURI;
-
-    constructor() ERC721("Troll", "Troll") Ownable(msg.sender) {}
+contract TestERC721 is ERC721, Ownable {
+    string private _baseTokenURI;
+    
+    constructor(string memory baseURI) ERC721("BasicNFT", "BNFT") Ownable(msg.sender)  {
+        _baseTokenURI = baseURI;
+    }
 
     function mint(address to, uint256 tokenId) external onlyOwner {
         _mint(to, tokenId);
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return _tokenURI;
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
     }
-
-    function setTokenURI(string memory newTokenURI) external onlyOwner {
-        _tokenURI = newTokenURI;
-    }
+    
 }
